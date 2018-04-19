25955b4388faf7c5f918545325a88551b9227a0a
Jijie Chen <jijie.chen@someplace.com>
2018-04-19 22:32:14 +0800

    Added more cases for move to keep it safe

--- ToyRobot.Tests/Commands/MoveCommandFacts.cs
+++ ToyRobot.Tests/Commands/MoveCommandFacts.cs
@@ -26,17 +26,43 @@ namespace ToyRobot.Tests.Commands
         
         
 
-        [Fact]
-        public void ShouldMoveNorth()
+        [Theory]
+        [InlineData(Direction.North, 2, 4)]
+        [InlineData(Direction.South, 2, 2)]
+        [InlineData(Direction.West, 1, 3)]
+        [InlineData(Direction.East, 3, 3)]
+        public void ShouldMoveForwardAndKeepDirection(Direction direction, int expectedX, int expectedY)
         {
-            var robot = new Robot();
-            robot.PlaceAt(new Coordinate(2,3), Direction.North);
+            var robot = new Robot(new Table(5, 5));
+            robot.PlaceAt(new Coordinate(2,3), direction);
 
             new MoveCommand().Execute(robot);
             
-            Assert.Equal(Direction.North, robot.Direction);
-            Assert.Equal(2, robot.Coordinate.X);
-            Assert.Equal(4, robot.Coordinate.Y);
+            Assert.Equal(direction, robot.Direction);
+            Assert.Equal(expectedX, robot.Coordinate.X);
+            Assert.Equal(expectedY, robot.Coordinate.Y);
+        }
+        
+        
+        [Theory]
+        [InlineData(2, 4, Direction.North)]
+        [InlineData(4, 4, Direction.North)]
+        [InlineData(1, 0, Direction.South)]
+        [InlineData(4, 0, Direction.South)]
+        [InlineData(0, 0, Direction.West)]
+        [InlineData(0, 4, Direction.West)]
+        [InlineData(4, 1, Direction.East)]
+        [InlineData(4, 4, Direction.East)]
+        public void ShouldNotMoveOutOfTable(int x, int y, Direction direction)
+        {
+            var robot = new Robot(new Table(5, 5));
+            robot.PlaceAt(new Coordinate(x, y), direction);
+
+            new MoveCommand().Execute(robot);
+            
+            Assert.Equal(direction, robot.Direction);
+            Assert.Equal(x, robot.Coordinate.X);
+            Assert.Equal(y, robot.Coordinate.Y);
         }
         
         
