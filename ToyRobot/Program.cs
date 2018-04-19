7db914367b34ac6d25ee0d495c2552fc35f927f0
Jijie Chen <jijie.chen@someplace.com>
2018-04-19 22:02:29 +0800

    First run! (Which is quite long. I should built the solution from top to bottom)

--- ToyRobot/Program.cs
+++ ToyRobot/Program.cs
@@ -6,7 +6,7 @@ namespace ToyRobot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CommandLineExecutor.Execute(Console.In, Console.Out);
         }
     }
 }
